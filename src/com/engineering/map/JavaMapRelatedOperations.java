ec70ea6383c6c57a5b57ce77e1e67bdcd29dc07a
Abhinav Trivedi <abhinavtrivedii@outlook.com>
2026-04-23 23:58:48 +0530

    Sorting MAP on basis of value , used custom object.

--- src/com/engineering/map/JavaMapRelatedOperations.java
+++ src/com/engineering/map/JavaMapRelatedOperations.java
@@ -41,6 +41,43 @@ class Marks {
 
 public class JavaMapRelatedOperations {
 
+    private static void SortBycalueUsingStreams(Map<String, Marks> hashMap) {
+        Map<String, Marks> sortedMap = hashMap.entrySet()
+                .stream()
+                .sorted(Comparator.comparingInt(e -> e.getValue().getMark()))
+                .collect(
+                        LinkedHashMap::new,
+                        (map, entry) -> map.put(entry.getKey(), entry.getValue()),
+                        LinkedHashMap::putAll
+                );
+
+        System.out.println("HashMap after sorting on basis of values using Streams : " + sortedMap);
+    }
+
+    private static void sortByValueLogic(Map<String, Marks> hashMap) {
+        List<Map.Entry<String ,Marks>> marksList = new ArrayList<>(hashMap.entrySet());
+
+        Collections.sort(marksList, new Comparator<Map.Entry<String, Marks>>() {
+            @Override
+            public int compare(Map.Entry<String, Marks> o1, Map.Entry<String, Marks> o2) {
+                return Integer.compare(o1.getValue().getMark(), o2.getValue().getMark());
+               // return o1.getValue().getSubject().compareTo(o2.getValue().getSubject());
+            }
+        });
+
+        /* marksList.sort((o1, o2) -> Integer.compare(o1.getValue().getMark(), o2.getValue().getMark()));*/
+        Map<String,Marks> linkedHashMap = new LinkedHashMap<>();
+        for(Map.Entry<String, Marks> entry : marksList){
+            linkedHashMap.put(entry.getKey(), entry.getValue());
+        }
+
+        System.out.println("HashMap after sorting on basis of values : " + linkedHashMap);
+
+        for(Map.Entry<String, Marks> entry : linkedHashMap.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue().getMark());
+        }
+    }
+
     static void main() {
         Marks m1 = new Marks("Maths", 98);
         Marks m2 = new Marks("Physics", 95);
@@ -52,26 +89,7 @@ public class JavaMapRelatedOperations {
         hashMap.put(m3.getSubject(), m3);
 
         // need to sort the map on basis of values.
-        List<Map.Entry<String ,Marks>> marksList = new ArrayList<>(hashMap.entrySet());
-
-        Collections.sort(marksList, new Comparator<Map.Entry<String, Marks>>() {
-            @Override
-            public int compare(Map.Entry<String, Marks> o1, Map.Entry<String, Marks> o2) {
-                return Integer.compare(o1.getValue().getMark(), o2.getValue().getMark());
-               // return o1.getValue().getSubject().compareTo(o2.getValue().getSubject());
-            }
-        });
-
-      /* marksList.sort((o1, o2) -> Integer.compare(o1.getValue().getMark(), o2.getValue().getMark()));*/
-        Map<String,Marks> linkedHashMap = new LinkedHashMap<>();
-        for(Map.Entry<String, Marks> entry : marksList){
-            linkedHashMap.put(entry.getKey(), entry.getValue());
-        }
-
-        System.out.println("HashMap after sorting on basis of values : " + linkedHashMap);
-
-        for(Map.Entry<String, Marks> entry : linkedHashMap.entrySet()){
-            System.out.println(entry.getKey() + " : " + entry.getValue().getMark());
-        }
+        sortByValueLogic(hashMap);
+        SortBycalueUsingStreams(hashMap);
     }
 }

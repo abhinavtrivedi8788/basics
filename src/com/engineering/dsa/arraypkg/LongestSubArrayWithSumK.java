e06c4c7138a519400f1070f848f33c7cc98bae2b
Abhinav Trivedi <abhinavtrivedii@outlook.com>
2026-04-23 15:45:20 +0530

    Optimal Solution for Longest SubArray for Sum K

--- src/com/engineering/dsa/arraypkg/LongestSubArrayWithSumK.java
+++ src/com/engineering/dsa/arraypkg/LongestSubArrayWithSumK.java
@@ -17,13 +17,38 @@ public class LongestSubArrayWithSumK {
         return maxLength;
     }
 
+    // Using Two Pointer approach
+    private static int longestSubArrayWithSumK_Optimal(int[] arr, int k) {
+        if (arr == null || arr.length == 0) return 0;
+        int maxLength=0;
+        int sum =0;
+        int leftPointer=0;
+        int rightPointer=0;
+
+        while(rightPointer<arr.length){
+            sum+=arr[rightPointer];
+            while(sum > k){
+                sum -= arr[leftPointer];
+                leftPointer++;
+            }
+            if(sum == k){
+                maxLength = Math.max(maxLength, rightPointer-leftPointer+1);
+            }
+            rightPointer++;
+        }
+        return maxLength;
+    }
+
 
     static void main() {
         int[] arr = {1, 2, 3, 7, 5, 6, 9};
-        int k = 15;
-        int maxLength = longestSubArrayWithSumK_BruteForce(arr, k);
-        System.out.println("Longest Sub Array with sum K : " + maxLength);
+        int k = 13;
+        System.out.println("Longest Sub Array with sum K - BRUTE FORCE : " + longestSubArrayWithSumK_BruteForce(arr, k));
+        System.out.println("Longest Sub Array with sum K - OPTIMAL : " + longestSubArrayWithSumK_Optimal(arr, k));
+
     }
+
+
 
 
 }

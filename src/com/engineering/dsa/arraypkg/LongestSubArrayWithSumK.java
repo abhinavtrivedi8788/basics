63e9dc00238a08961c6b51b8405d4b0f2ac02a12
Abhinav Trivedi <abhinavtrivedii@outlook.com>
2026-04-23 15:59:41 +0530

    Optimal Solution for Longest SubArray for Sum K ( added striver code )

--- src/com/engineering/dsa/arraypkg/LongestSubArrayWithSumK.java
+++ src/com/engineering/dsa/arraypkg/LongestSubArrayWithSumK.java
@@ -1,5 +1,7 @@
 package com.engineering.dsa.arraypkg;
 
+import java.time.Duration;
+
 public class LongestSubArrayWithSumK {
 
     private static int longestSubArrayWithSumK_BruteForce(int[] arr, int k) {
@@ -17,7 +19,7 @@ public class LongestSubArrayWithSumK {
         return maxLength;
     }
 
-    // Using Two Pointer approach
+    // Using Two Pointer approach - my approach
     private static int longestSubArrayWithSumK_Optimal(int[] arr, int k) {
         if (arr == null || arr.length == 0) return 0;
         int maxLength=0;
@@ -40,11 +42,36 @@ public class LongestSubArrayWithSumK {
     }
 
 
+    private static int byStriver(int[] arr, int k) {
+        if (arr == null || arr.length == 0) return 0;
+        int leftPointer=0;
+        int rightPointer=0;
+        int maxLength=0;
+        int sum =arr[0];
+        while(rightPointer<arr.length){
+            while(leftPointer <= rightPointer && sum > k){
+                sum -= arr[leftPointer];
+                leftPointer++;
+            }
+            if(sum == k){
+                maxLength = Math.max(maxLength, rightPointer-leftPointer+1);
+            }
+            rightPointer++;
+            if(rightPointer < arr.length){
+                sum += arr[rightPointer];
+            }
+        }
+        return maxLength;
+    }
+
+
     static void main() {
         int[] arr = {1, 2, 3, 7, 5, 6, 9};
         int k = 13;
         System.out.println("Longest Sub Array with sum K - BRUTE FORCE : " + longestSubArrayWithSumK_BruteForce(arr, k));
         System.out.println("Longest Sub Array with sum K - OPTIMAL : " + longestSubArrayWithSumK_Optimal(arr, k));
+        System.out.println("Longest Sub Array with sum K - DUMMY : " + byStriver(arr, k));
+
 
     }
 

d03e3d74710dff1915357546873a63240d4d5158
Abhinav Trivedi <abhinavtrivedii@outlook.com>
2026-04-26 14:50:05 +0530

    to check if product of elements in array is positive or negative or Zero

--- src/com/engineering/dsa/arraypkg/ArrayElementMultiplication.java
+++ src/com/engineering/dsa/arraypkg/ArrayElementMultiplication.java
@@ -2,7 +2,7 @@ package com.engineering.dsa.arraypkg;
 
 public class ArrayElementMultiplication {
 
-    private static int findIfMultiplicationOfElementsIsPostiveNegativeOrZero(int[] array) {
+    private static int findIfMultiplicationOfElementsIsPositiveNegativeOrZero(int[] array) {
         int counter =0;
         for(int number : array){
             if(number == 0) return 0;
@@ -23,9 +23,9 @@ public class ArrayElementMultiplication {
         int [] array1 = {25,-5,-6,-2,5};
         int [] array2 = {25,5,-6,-2,5};
         int [] array3 = {25,5,6,-2,0};
-        System.out.println("Product of all elements in array 1 is : "+findIfMultiplicationOfElementsIsPostiveNegativeOrZero(array1));
-        System.out.println("Product of all elements in array 2 is :  "+findIfMultiplicationOfElementsIsPostiveNegativeOrZero(array2));
-        System.out.println("Product of all elements in array 3 is :  "+findIfMultiplicationOfElementsIsPostiveNegativeOrZero(array3));
+        System.out.println("Product of all elements in array 1 is : "+findIfMultiplicationOfElementsIsPositiveNegativeOrZero(array1));
+        System.out.println("Product of all elements in array 2 is :  "+findIfMultiplicationOfElementsIsPositiveNegativeOrZero(array2));
+        System.out.println("Product of all elements in array 3 is :  "+findIfMultiplicationOfElementsIsPositiveNegativeOrZero(array3));
     }
 
 }

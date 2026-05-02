fef88c3e8f4f734b7b4681a908349545e11bc1cf
Abhinav Trivedi <abhinavtrivedii@outlook.com>
2026-05-02 20:27:09 +0530

    Missing number in Array using Xor and sum

--- src/com/engineering/dsa/arraypkg/FindMissingNumber.java
+++ src/com/engineering/dsa/arraypkg/FindMissingNumber.java
@@ -19,6 +19,7 @@ public class FindMissingNumber {
         return -1;
     }
 
+    // TC - O(n) , SP - O(1)
     private static int findMissingNumberUsingOptimalSolution(int[] array) {
         if(array.length == 0) return 0;
         int n = array.length + 1;
@@ -30,13 +31,28 @@ public class FindMissingNumber {
         return sumOfN - sumOfArray;
     }
 
+    // TC - O(n) , SP - O(1)
+    private static int findMissingNumberUsingXOR(int[] array) {
+        int xor = 0;
+        for (int i = 1; i <= array.length + 1; i++) {
+            xor ^= i;
+        }
+        for (int num : array) {
+            xor ^= num;
+        }
+        return xor;
+    }
+
     static void main() {
-        int [] array = new int[] {1,2,3,4,6,7};
+        int [] array = new int[] {1,2,3,4,6,7}; // 8,2,4,5,3,7,1
         int missingNumberUsingBruteForce = findMissingNumberUsingBruteForce(array);
         System.out.println("Missing Number Using Brute Force : "+missingNumberUsingBruteForce);
 
         int missingNumberUsingOptimalSolution = findMissingNumberUsingOptimalSolution(array);
         System.out.println("Missing Number Using Optimal Solution : "+ missingNumberUsingOptimalSolution);
+
+        int missingNumberUsingXOR = findMissingNumberUsingXOR(array);
+        System.out.println("Missing Number Using XOR : "+ missingNumberUsingXOR);
     }
 
 
